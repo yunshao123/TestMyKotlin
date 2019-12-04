06e568c42d2e6d2d29457d582ca28ca21b28956f
49545 <495454126@qq.com>
2019-12-04 23:31:24 +0800

    123

--- app/src/main/java/com/example/lan/testmykotlin/MainActivity.kt
+++ app/src/main/java/com/example/lan/testmykotlin/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.lan.testmykotlin
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
@@ -12,27 +14,28 @@ import java.util.*
 
 
 //https://github.com/teddyisme/LWidgets
-class MainActivity : AppCompatActivity(){
-    private var menuItem:MenuItem?=null
-    private val mBottomNavigationView = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        menuItem=item
-        when (item.itemId) {
+class MainActivity : AppCompatActivity() {
+    private var menuItem: MenuItem? = null
+    private val mBottomNavigationView =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            menuItem = item
+            when (item.itemId) {
 
-            R.id.menu -> {
+                R.id.menu -> {
 
-                vp_main.currentItem = 0
+                    vp_main.currentItem = 0
 
-                return@OnNavigationItemSelectedListener true
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.more -> {
+
+                    vp_main.currentItem = 1
+
+                    return@OnNavigationItemSelectedListener true
+                }
             }
-            R.id.more -> {
-
-                vp_main.currentItem = 1
-
-                return@OnNavigationItemSelectedListener true
-            }
+            false
         }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +49,7 @@ class MainActivity : AppCompatActivity(){
             fragmentArrayList
         )
         vp_main.adapter = adapter
-        vp_main.addOnPageChangeListener(object:ViewPager.OnPageChangeListener{
+        vp_main.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageSelected(position: Int) {
                 if (menuItem != null) {
                     menuItem!!.isChecked = false
@@ -54,10 +57,14 @@ class MainActivity : AppCompatActivity(){
                     bottom_navigation.menu.getItem(0).isChecked = false
                 }
                 menuItem = bottom_navigation.menu.getItem(position)
-                menuItem!!.isChecked=true
+                menuItem!!.isChecked = true
             }
 
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
 
             }
 
@@ -71,11 +78,15 @@ class MainActivity : AppCompatActivity(){
         menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onMenuOpened(featureId: Int, menu: Menu?): Boolean {
         if (menu != null) {
             if (menu.javaClass.simpleName.equals("MenuBuilder", ignoreCase = true)) {
                 try {
-                    val method = menu.javaClass.getDeclaredMethod("setOptionalIconsVisible", java.lang.Boolean.TYPE)
+                    val method = menu.javaClass.getDeclaredMethod(
+                        "setOptionalIconsVisible",
+                        java.lang.Boolean.TYPE
+                    )
                     method.isAccessible = true
                     method.invoke(menu, true)
                 } catch (e: Exception) {
@@ -86,12 +97,14 @@ class MainActivity : AppCompatActivity(){
         }
         return super.onMenuOpened(featureId, menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
             R.id.change_user -> {
-
+                showList()
             }
             else -> {
+                Toast.makeText(this, "dddd", Toast.LENGTH_SHORT).show()
             }
         }
         return super.onOptionsItemSelected(item)
@@ -104,5 +117,17 @@ class MainActivity : AppCompatActivity(){
         fragmentArrayList!!.add(FragmentOne())
     }
 
-
+    private fun showList() {
+        var builder: AlertDialog.Builder
+        val items = arrayOf("item 1", "item 2", "item 3", "item 4", "item 5", "item 6")
+        builder = AlertDialog.Builder(this)
+            .setItems(items) { _, i ->
+                Toast.makeText(
+                    this@MainActivity,
+                    "你点击的内容为： " + items[i],
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        builder.create().show()
+    }
 }
